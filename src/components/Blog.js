3472fd7f33018d9745a1ae7258ba1c5ea394c04a
Atte Laakso <laakso95@gmail.com>
2019-07-22 13:17:52 +0300

    Exercise 5.6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+
+  const [showExtended, setShowExtended] = useState(false)
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  const blogElement = () => {
+    return (
+      <div style={blogStyle}>
+        <div onClick={() => setShowExtended(!showExtended)}>
+          {blog.title} {blog.author}
+        </div>
+        {showExtended &&
+          <div>
+            <a href={blog.url}>{blog.url}</a>
+            <p>{blog.likes} likes <button onClick={() => console.log('++ likes')}>likes</button></p>
+            <p>added by {blog.user.name}</p>
+          </div>
+        }
+      </div>
+    )
+  }
+
+  return (
+    blogElement()
+  )
+}
 
 export default Blog
